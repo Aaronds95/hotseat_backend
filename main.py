67bb91a1f6c1e144575a133a99adb41852ea0ff3
Aaron Schultheis <aaronds95@gmail.com>
2025-07-30 17:35:38 -0400

    Added post options

--- main.py
+++ main.py
@@ -2,8 +2,25 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+userEnvSelection = ""
+userGoalSelection = ""
+userMoodSelection = ""
+
+
 @app.get("/")
 def read_root():
     return {"message": "Hello Srikar, how's Portugal?"}
 
+@app.post("/items/")
+def create_item(name: str, price: float):
+    return {"name:" : name, "price": price}
 
+
+@app.post("/selection")
+def select_env(envSelection: str, goalSelection: str, moodSelection: str):
+    
+    userEnvSelection = envSelection
+    userGoalSelection = goalSelection
+    userMoodSelection = moodSelection
+
+    return {"envSelection" : userEnvSelection, "goalSelection" : userGoalSelection, "moodSelection" : userMoodSelection}
