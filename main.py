a7304ab8571a90f46ae04a1f3072bf827bcea3c0
Aaron Schultheis <aaronds95@gmail.com>
2025-08-04 16:49:27 -0400

    Added new route for question generation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from openai_client import generate_games
+from openai_client import generate_games, generate_questions
 from typing import Optional
 import json
 
@@ -16,15 +16,21 @@ class GameOut(BaseModel):
     gameInstructions: str
     gameExamples: Optional[str]
 
-class SelectionResponse(BaseModel):
+class GameResponse(BaseModel):
     games: list[GameOut]
+
+class QuestionOut(BaseModel):
+    questionText: str
+
+class QuestionResponse(BaseModel):
+    questions: list[QuestionOut]
 
 
 @app.get("/")
 def read_root():
     return {"message": "Hello Srikar, how's Portugal?"}
 
-@app.post("/games", response_model=SelectionResponse)
+@app.post("/games", response_model=GameResponse)
 def select_env(selection: SelectionIn):
     data = generate_games(
         selection.envSelection, selection.goalSelection, selection.moodSelection
@@ -32,3 +38,12 @@ def select_env(selection: SelectionIn):
     games = data['games']
 
     return {"games": games}
+
+@app.post("/questions", response_model=QuestionResponse)
+def select_env(selection: SelectionIn):
+    data = generate_questions(
+        selection.envSelection, selection.goalSelection, selection.moodSelection
+    )
+    questions = data['questions']
+
+    return {"questions": questions}
--- openai_client.py
+++ openai_client.py
@@ -6,7 +6,8 @@ import json
 load_dotenv()
 
 _client   = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-_PROMPTID = os.getenv("HOTSEAT_GAMES_PROMPT_ID")               # <— NEW
+_GAMESPROMPTID = os.getenv("HOTSEAT_GAMES_PROMPT_ID")
+_QUESTIONSPROMPTID = os.getenv("HOTSEAT_QUESTIONS_PROMPT_ID")
 
 client = OpenAI()
 
@@ -14,8 +15,35 @@ def generate_games(env, goal, mood):
 
     response = client.responses.create(
     prompt={
-        "id": _PROMPTID,
+        "id": _GAMESPROMPTID,
         "version": "3",
+        "variables": {
+        "userenvselection": env,
+        "usergoalselection": goal,
+        "usermoodselection": mood
+        }
+    },
+    input=[{
+        "role": "user",
+        "content": "Please return the result in JSON."
+        }],
+    reasoning={},
+    max_output_tokens=2048,
+    store=True
+    )
+
+    json_string = response.output[0].content[0].text
+    data = json.loads(json_string)
+
+    return data
+
+
+def generate_questions(env, goal, mood):
+
+    response = client.responses.create(
+    prompt={
+        "id": _QUESTIONSPROMPTID,
+        "version": "1",
         "variables": {
         "userenvselection": env,
         "usergoalselection": goal,
